c6b9a9aab274ceb9a17f4581186049834d48a52e
Naoya Yamashita <conao3@gmail.com>
2021-02-13 06:10:08 +0900

    install oauth2

--- oauth2-request.el
+++ oauth2-request.el
@@ -5,7 +5,7 @@
 ;; Author: Naoya Yamashita <conao3@gmail.com>
 ;; Version: 0.0.1
 ;; Keywords: convenience
-;; Package-Requires: ((emacs "26.1"))
+;; Package-Requires: ((emacs "26.1") (oauth2 "0.14"))
 ;; URL: https://github.com/conao3/oauth2-request.el
 
 ;; This program is free software: you can redistribute it and/or modify
@@ -27,6 +27,8 @@
 
 
 ;;; Code:
+
+(require 'oauth2)
 
 (defgroup oauth2-request nil
   "OAuth2 request package interface."
