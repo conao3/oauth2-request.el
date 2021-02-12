5dbb9d77b0364d5bb3c5ce41cda1fac2be8d9d59
Naoya Yamashita <conao3@gmail.com>
2021-02-13 06:09:10 +0900

    generate project

---
+++ oauth2-request-tests.el
@@ -0,0 +1,34 @@
+;;; oauth2-request-tests.el --- Test definitions for oauth2-request  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; URL: https://github.com/conao3/oauth2-request.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Test definitions for `oauth2-request'.
+
+
+;;; Code:
+
+(require 'cort)
+(require 'oauth2-request)
+
+
+;; (provide 'oauth2-request-tests)
+
+;;; oauth2-request-tests.el ends here
